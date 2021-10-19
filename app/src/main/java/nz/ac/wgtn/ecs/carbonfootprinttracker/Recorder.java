07dde9bc61e37a307d08ed1bbeb716a8e99d30fe
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-19 15:29:46 +1300

    Splash Page Layout - Done Carbon recorder Page - Added

---
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprinttracker/Recorder.java
@@ -0,0 +1,14 @@
+package nz.ac.wgtn.ecs.carbonfootprinttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recorder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorder);
+    }
+}
