190329f82cd3b6bf887e302c99f1e7172f083816
liuziyu <liuziyu@vuw.ac.nz>
2021-10-20 15:48:26 +1300

    deleted errors in login activity

--- app/src/main/java/nz/ac/wgtn/ecs/carbonfootprinttracker/setupActivity.java
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprinttracker/setupActivity.java
@@ -11,8 +11,7 @@ public class setupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
-        WebView webview = findViewById(R.id.webview);
-        webview.loadUrl("www.google.com");
+
     }
 
 
